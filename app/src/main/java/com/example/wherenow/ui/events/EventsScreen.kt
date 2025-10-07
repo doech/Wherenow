be6ac735cba4f18758b72bd5625c65125f4ca4d7
doech <sie24405@uvg.edu.gt>
2025-10-07 09:35:18 -0600

    estructura para nueva fase

---
+++ app/src/main/java/com/example/wherenow/ui/events/EventsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.wherenow.ui.events
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun EventsScreen(navController: NavController) {
+    // Pantalla vacía
+    //emily y ale s
+}
